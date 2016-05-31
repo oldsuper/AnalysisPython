b3e64c73ab51ecf7274ce6f04da519c5568f4529
gaosongbo <oldsuper@hotmail.com>
2016-05-31 18:37:40 +0800

    udpate

--- src/main.py
+++ src/main.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python
 # coding=utf8
 __author__ = 'gaosongbo'
+def init():
+    import ConfigParser
+    conf=ConfigParser.ConfigParser()
+    conf.read('../conf/ana.properties')
+    print conf.sections()
+if __name__=="__main__":
+    init()
